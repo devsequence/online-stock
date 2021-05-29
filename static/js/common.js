a25483e95ac9ec40ae669a54daabcfd047552160
Slipak Bohdan <fear3494@gmail.com>
2021-05-29 03:22:02 +0300

    git init

--- static/js/common.js
+++ static/js/common.js
@@ -8,41 +8,6 @@ $(window).on('scroll', function() {
         $header.removeClass('scroll-nav');
     }
 });
-// $(document).mouseup(function (e){
-//     var div = $('.header-lang');
-//     if (!div.is(e.target)
-//         && div.has(e.target).length === 0) {
-//         $(div).removeClass('open');
-//     }
-// });
-// $('.header-lang__title').on('click', function (e) {
-//     var $this = $(this);
-//     var $thisParent  = $this.parent();
-//     $thisParent.toggleClass('open');
-// });
-//
-// $('.header-btn').on('click', function (e) {
-//     var $this = $(this);
-//     $this.toggleClass('open');
-//     $('.header-mobile').toggleClass('open');
-//     $('body').toggleClass('scroll-h');
-//     $('.header-overlay').toggleClass('open');
-// });
-//
-// $('.header-overlay').on('click', function (e) {
-//     var $this = $(this);
-//     $this.toggleClass('open');
-//     $('.header-mobile').toggleClass('open');
-//     $('body').toggleClass('scroll-h');
-//     $('.header-btn').toggleClass('open');
-// });
-//
-// var swiper = new Swiper(".hero-slider", {
-//     pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true,
-//     },
-// });
 $('.header-btn').on('click', function (e) {
     var $this = $(this);
     $this.toggleClass('open').find('.icon').toggleClass('hidden');
