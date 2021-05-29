bf2b0c253536e36b4ff1f93ae7439df76b572710
Slipak Bohdan <fear3494@gmail.com>
2021-05-29 03:13:10 +0300

    git init

--- static/js/common.js
+++ static/js/common.js
@@ -46,7 +46,17 @@ $(window).on('scroll', function() {
 $('.header-btn').on('click', function (e) {
     var $this = $(this);
     $this.toggleClass('open').find('.icon').toggleClass('hidden');
-    $('.header-mobile').toggleClass('open');
+    $('.header .btn-register, .header-nav').toggleClass('open');
     $('body').toggleClass('scroll-h');
-    $('.header-overlay').toggleClass('open');
+});
+$('.header-nav a').click(function(e) {
+    var headerHeight = $('header').outerHeight(); // Target your header navigation here
+    var targetHref   = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(targetHref).offset().top - headerHeight // Add it to the calculation here
+    }, 1000);
+    $('.header-btn').toggleClass('open').find('.icon').toggleClass('hidden');
+    $('.header .btn-register, .header-nav').toggleClass('open');
+    $('body').removeClass('scroll-h');
+    e.preventDefault();
 });
